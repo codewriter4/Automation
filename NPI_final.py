27a5d76c1244a27c27c438911c6efe922887d896
Code_Writer <codewriter404@gmail.com>
2017-12-02 15:59:30 -0700

    added website to header commment

--- NPI_final.py
+++ NPI_final.py
@@ -2,7 +2,7 @@
 '''Author: Tom Rafter
 Description: Uses Selenium to webscrape an adjacent file containing
 a lisk of firstname, last name in an defined array(i.e. providers=[('John','Doe'),('Jane','Doe')])
-Wrote this to scape provider NPI #s for a healthcare organization to improve data quality
+Wrote this to scape provider NPI #s for a healthcare organization to improve data quality.
 '''
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
